515e6b2abcd9bd51098e5b46efdadb64d03055f8
Alonso <alon.miranda.m@gmail.com>
2024-09-09 23:54:24 -0300

    cambios final

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { addIcons } from 'ionicons';
-import { logoApple } from 'ionicons/icons';
 import { ChatService } from './chat.service';
+
+
 
 @Component({
   selector: 'app-chat',
@@ -11,62 +11,65 @@ import { ChatService } from './chat.service';
 })
 
 
-export class ChatPage implements OnInit {
-  //Los mensajes del bot y del usuario se van acumulando en el array "messages" en forma de objetos.
-  //Cada objeto tiene dos propiedades: El contenido del mensaje, y fromUser (true = usuario / falsa=bot)
-  //este proceso de acumulación es gestionado por el método push() en sendMessage
-  messages = [
-    { text: 'Hola, ¿En que puedo ayudarte?', fromUser: false }
-  ];
-
-
+export class ChatPage{
+  //propiedades de la clase ChatPage
 
   userMessage: string = '';
 
   constructor(private router: Router, private chatService: ChatService) { 
-    addIcons({ logoApple });
-  }
-
-  ngOnInit() { }
-
-  libreta() {
-    this.router.navigate(['/libreta']);
   }
 
 
-  login(){
-    this.router.navigate(['/login']);
-  }
-  
+    //Los mensajes del bot y del usuario se van acumulando en el array "messages" en forma de objetos.
+    //Cada objeto tiene dos propiedades: El contenido del mensaje, y fromUser (true = usuario / falsa=bot)
+    //este proceso de acumulación es gestionado por el método push() en sendMessage
+    messages = [
+      { text: 'Hola, ¿En que puedo ayudarte?', fromUser: false }
+    ];
 
-  //enviar el mensaje, trim elimina espacios en blanco, si esta vacio no se envia. 
-  sendMessage() {
-    if (this.userMessage.trim().length > 0) {
-      this.messages.push({ text: this.userMessage, fromUser: true });
 
-       // Enviar el mensaje al LLM (chatbot)
-      this.chatService.sendMessageToLLM(this.userMessage).subscribe(
-        (response) => {
-          const botReply = response.choices[0].message.content;
-          this.messages.push({ text: botReply, fromUser: false });  // Añadir la respuesta del bot
-        },
-        (error) => {
-          console.error('Error enviando mensaje al LLM', error);
-          this.messages.push({ text: 'Error al comunicarse con el LLM.', fromUser: false });
-        }
-      );
+    //Metodos de la clase ChatPage
 
-      this.userMessage = '';// Limpiar el input después de enviar
+    libreta() {
+      this.router.navigate(['/libreta']);
     }
-  }
 
-  attachFile() {
-    console.log('Adjuntar archivo');
-    // Implementación futura para adjuntar archivos
-  }
 
-  sendAudio() {
-    console.log('Enviar audio');
-    // Implementación futura para enviar audio
-  }
+    login(){
+      this.router.navigate(['/login']);
+    }
+    
+
+    //enviar el mensaje, trim elimina espacios en blanco, si esta vacio no se envia. 
+    sendMessage() {
+      if (this.userMessage.trim().length > 0) {
+        this.messages.push({ text: this.userMessage, fromUser: true });
+
+        // Enviar el mensaje al LLM (chatbot)
+        this.chatService.sendMessageToLLM(this.userMessage).subscribe(
+          (response) => {
+            const botReply = response.choices[0].message.content;
+            this.messages.push({ text: botReply, fromUser: false });  // Añadir la respuesta del bot
+          },
+          (error) => {
+            console.error('Error enviando mensaje al LLM', error);
+            this.messages.push({ text: 'Error al comunicarse con el LLM.', fromUser: false });
+          }
+        );
+
+        this.userMessage = '';// Limpiar el input después de enviar
+      }
+    }
+
+    adjuntarArchivo() {
+      alert('Adjuntar archivo');
+      // Implementación futura para adjuntar archivos
+    }
+
+    sendAudio() {
+      alert('Enviar audio');
+      // Implementación futura para enviar audio
+    }
+
+
 }
--- src/app/recover-key/recover-key.page.ts
+++ src/app/recover-key/recover-key.page.ts
@@ -1,46 +1,42 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
 
-@Component({
+
+
+@Component({/*Decorador @Component: Define la clase declarada en el código, como un componente Angular, configurando su comportamiento y apariencia. */
   selector: 'app-recover-key',
   templateUrl: './recover-key.page.html',
   styleUrls: ['./recover-key.page.scss'],
 })
 
 
+
 export class RecoverKeyPage {
+
+  //propiedades de la clase 
+
   email: string = '';
 
-  constructor(private router: Router, private alertController: AlertController) {}
-
-
+  constructor(private router: Router) {}
 
   sendRecoveryEmail() {
     if (!this.isValidEmail(this.email)) {
-      this.showAlert('Error', 'Por favor, ingrese un correo electrónico válido.');
+      alert('Por favor, ingrese un correo electrónico válido.');
       return;
     }
 
-    // Aquí iría la lógica para enviar el enlace de recuperación al correo electrónico
-
-    this.showAlert('Éxito', 'La contraseña de recuperación se ha enviado a su correo electrónico, por favor revise su bandeja de entrada.');
+    
+    // Aquí iría la lógica para enviar el enlace de recuperacion al email
+    alert('La contraseña de recuperación se ha enviado a su correo electrónico, por favor revise su bandeja de entrada.');
   }
 
-  // Validar formato de correo electrónico con expresiones regulares
-  isValidEmail(email: string): boolean {
+  // Validar formato de correo electrónico con expresion regular
+  isValidEmail(email: string): boolean { //toma un parámetro email de tipo string y devuelve un valor de tipo boolean. 
     const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return re.test(String(email).toLowerCase());
+    return re.test(String(email).toLowerCase()); //Metodo .test devolvera True al cumplir o viceversa
   }
 
-  showAlert(header: string, message: string) {
-    this.alertController.create({
-      header: header,
-      message: message,
-      buttons: ['OK']
-    }).then(alert => alert.present());
-  }
-
+ 
   //Funcion para redireccionar al login
   goToLogin() {
     this.router.navigate(['/login']);
