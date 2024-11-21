ff0b38a6ece3402b425dc2cccf8d073e195ae3e8
jeromephillip23 <je.bellenger@duocuc.cl>
2024-11-21 11:55:14 -0300

    quienessomos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,37 +2,57 @@
 
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard'; // Importa el guardia de autenticación si es necesario
 
 const routes: Routes = [
+  // Redirección por defecto a login
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Ruta de registro
   {
     path: 'register',
     loadChildren: () =>
       import('./register/register.module').then((m) => m.RegisterPageModule),
   },
+
+  // Ruta de login
   {
     path: 'login',
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginPageModule),
   },
+
+  // Ruta de chat
   {
     path: 'chat',
     loadChildren: () =>
       import('./chat/chat.module').then((m) => m.ChatPageModule),
     // Sin AuthGuard para invitados
   },
+
+  // Ruta de libreta (protegida con autenticación)
   {
     path: 'libreta',
     loadChildren: () =>
       import('./libreta/libreta.module').then((m) => m.LibretaPageModule),
-    canActivate: [AuthGuard], // Protegida
+    canActivate: [AuthGuard], // Protegida por autenticación
   },
+
+  // Ruta para recuperación de contraseña
   {
     path: 'recover-key',
     loadChildren: () =>
       import('./recover-key/recover-key.module').then(
         (m) => m.RecoverKeyPageModule
+      ),
+  },
+
+  // Ruta para la página "Quiénes somos"
+  {
+    path: 'quienes-somos',
+    loadChildren: () =>
+      import('./quienes-somos/quienes-somos.module').then(
+        (m) => m.QuienesSomosPageModule
       ),
   },
 ];
